6479daf9d095c66a91e022c0b767c7651c5d70fb
EnderIO1710 <Dallick1234@yandex.ru>
2025-08-02 14:08:23 +0300

    create bash-cript for server_performance_stats

--- server_performance_stats/server_stat.sh
+++ server_performance_stats/server_stat.sh
@@ -4,18 +4,31 @@ echo "######################"
 echo "# System Uptime Info #"
 echo "######################"
 
-echo "###################"
+uptime
+echo
+
+
 echo "# Total CPU Usage #"
-echo "###################"
 
-echo "######################"
+top -bn1 | grep "%Cpu(s):" | cut -d ',' -f 4 | awk '{print "Usage: " 100-$1 "%"}'
+
+echo
+
+
 echo "# Total Memory Usage #"
-echo "######################"
 
-echo "####################"
+free | grep "Mem:" -w | awk '{printf "MaxMem: %.1fGi\nUsedMem: %.1fGi (%.2f%%)\nFreeMem: %.1fGi (%.2f%%)\n",$2/1024^2, $3/1024^2, $3/$2 * 100, $4/1024^2, $4/$2 * 100}'
+
+echo
+
 echo "# Total Disk Usage #"
-echo "####################"
 
-echo "###################################"
+df -h | grep "/" -w | awk '{printf "Total: %sG\nUsed: %s (%.2f%%)\nFree: %s (%.2f%%)\n",$3 + $4, $3, $3/($3+$4) * 100, $4, $4/($3+$4) * 100}'
+
+echo
+
 echo "# Top 5 Processes by Memory Usage #"
-echo "###################################"
+
+ps aux --sort -%mem | head -n 6 | awk '{print $1 "\t" $2 "\t" $4 "\t" $4 "\t" $11}'
+
+echo
